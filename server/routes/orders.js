99e20caf8bb9244cb49ddfc8789cc5e46b433e4c
WALKME\gilad.l <gilad.l@walkme.com>
2018-08-05 16:26:19 +0300

    .

---
+++ server/routes/orders.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var passport = require('passport');
+var Order = require('../models/order');
+var authenticationMiddleware = require('../middlewares/authentication');
+
+var router = express.Router();
+
+router.route('/')
+    .get(authenticationMiddleware(),
+    function(req, res) {
+        Order.getAll()
+        .then(function(orders) {
+            res.send(orders);
+        })
+    }
+);
+
+router.route('/supplier/:id')
+    .get(authenticationMiddleware(),
+    function(req, res) {
+        Order.getAllBySupplierId(req.params.id)
+        .then(function(orders) {
+            res.send(orders);
+        })
+    }
+);
+
+router.route('/reciver/:id')
+    .get(authenticationMiddleware(),
+    function(req, res) {
+        Order.getAllByReciverId(req.params.id)
+        .then(function(orders) {
+            res.send(orders);
+        })
+    }
+);
+
+module.exports = router;
